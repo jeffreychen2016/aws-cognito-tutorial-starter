a7fcc68db44fc4c267ee653a45a001382f162bd6
Jeffrey Chen <jeffreychen@BNAJCHENMAC.local>
2020-06-29 21:29:14 -0500

    feat: add Cognito config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bulma/css/bulma.min.css';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bulma/css/bulma.min.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// register the Cognito to the client app
+// so that when app starts
+// it knows how to connect to correct Coginto user pool
+import config from "./config.json";
+import Amplify from "aws-amplify";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignId: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
