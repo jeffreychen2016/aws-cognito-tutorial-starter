15ee1ac7ba2f564bda0a7219d685b420be21cc04
Jeffrey Chen <jeffreychen@BNAJCHENMAC.local>
2020-07-07 20:28:48 -0500

    feat: add login and logout function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Auth } from "aws-amplify";
 
 export default class Navbar extends Component {
   render() {
+    // add logout
+    const handleLogOut = async (event) => {
+      event.preventDefault();
+      Auth.signOut();
+
+      this.props.auth.setAuthStatus(false);
+      this.props.auth.setUser(null);
+      try {
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
           <a className="navbar-item" href="/">
-            <img src="hexal-logo.png" width="112" height="28" alt="hexal logo" />
+            <img
+              src="hexal-logo.png"
+              width="112"
+              height="28"
+              alt="hexal logo"
+            />
           </a>
         </div>
 
@@ -25,18 +44,39 @@ export default class Navbar extends Component {
 
           <div className="navbar-end">
             <div className="navbar-item">
+              {/* add conditional rendering here */}
+              {this.props.auth.isAuthenticated && this.props.auth.user && (
+                <p>Hello {this.props.auth.user.username}</p>
+              )}
+
               <div className="buttons">
-                <a href="/register" className="button is-primary">
-                  <strong>Register</strong>
-                </a>
-                <a href="/login" className="button is-light">
-                  Log in
-                </a>
+                {/* add conditional rendering here */}
+                {!this.props.auth.isAuthenticated && (
+                  <div>
+                    <a href="/register" className="button is-primary">
+                      <strong>Register</strong>
+                    </a>
+                    <a href="/login" className="button is-light">
+                      Log in
+                    </a>
+                  </div>
+                )}
+                {this.props.auth.isAuthenticated && (
+                  <div>
+                    <a
+                      href="/"
+                      onClick={handleLogOut}
+                      className="button is-light"
+                    >
+                      Log out
+                    </a>
+                  </div>
+                )}
               </div>
             </div>
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
--- src/components/auth/LogIn.js
+++ src/components/auth/LogIn.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import FormErrors from "../FormErrors";
 import Validate from "../utility/FormValidation";
+import { Auth } from "aws-amplify";
 
 class LogIn extends Component {
   state = {
@@ -8,20 +9,20 @@ class LogIn extends Component {
     password: "",
     errors: {
       cognito: null,
-      blankfield: false
-    }
+      blankfield: false,
+    },
   };
 
   clearErrorState = () => {
     this.setState({
       errors: {
         cognito: null,
-        blankfield: false
-      }
+        blankfield: false,
+      },
     });
   };
 
-  handleSubmit = async event => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
     // Form validation
@@ -29,16 +30,36 @@ class LogIn extends Component {
     const error = Validate(event, this.state);
     if (error) {
       this.setState({
-        errors: { ...this.state.errors, ...error }
+        errors: { ...this.state.errors, ...error },
       });
     }
 
     // AWS Cognito integration here
+    try {
+      const user = await Auth.signIn(this.state.username, this.state.password);
+
+      this.props.auth.setAuthStatus(true);
+      this.props.auth.setUser(user);
+
+      console.log(user);
+
+      // redirect to /
+      this.props.history.push("/");
+    } catch (error) {
+      let err = null;
+      !error.message ? (err = { message: error }) : (err = error);
+      this.setState({
+        errors: {
+          ...this.state.errors,
+          cognito: err,
+        },
+      });
+    }
   };
 
-  onInputChange = event => {
+  onInputChange = (event) => {
     this.setState({
-      [event.target.id]: event.target.value
+      [event.target.id]: event.target.value,
     });
     document.getElementById(event.target.id).classList.remove("is-danger");
   };
@@ -53,8 +74,8 @@ class LogIn extends Component {
           <form onSubmit={this.handleSubmit}>
             <div className="field">
               <p className="control">
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="text"
                   id="username"
                   aria-describedby="usernameHelp"
@@ -66,8 +87,8 @@ class LogIn extends Component {
             </div>
             <div className="field">
               <p className="control has-icons-left">
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="password"
                   id="password"
                   placeholder="Password"
@@ -86,9 +107,7 @@ class LogIn extends Component {
             </div>
             <div className="field">
               <p className="control">
-                <button className="button is-success">
-                  Login
-                </button>
+                <button className="button is-success">Login</button>
               </p>
             </div>
           </form>
